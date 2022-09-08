8a96a35f3b43bbde67578b4e4246ea1db51023f4
IgorGuariroba <1g0r.guari@gmail.com>
2022-09-07 22:02:04 -0300

    Terminado a stilização dos horarios disponiveis

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Input from "../../components/Input";
 import PageHeader from "../../components/pageHeader";
+import Select from "../../components/select";
 import TeacherItem from "../../components/TeacherItem";
 
 import './styles.css';
@@ -11,8 +12,32 @@ function TeacherList() {
       <PageHeader title="Estes são os proffys diponiveis.">
         <form id="search-teachers">
 
-          <Input name="subject" label="Matéria" />
-          <Input name="week_day" label="Dia da semana" />
+        <Select 
+          name="subject" 
+          label="Materia" 
+          options={[
+            {value: '1', label: 'Mecanico => Carros'},
+            {value: '2', label: 'Borracheiro => Carros'},
+            {value: '3', label: 'Pintor => Carros'},
+            {value: '4', label: 'Pintor => Casa'},
+            {value: '5', label: 'Jardineiro => condomínios'},
+            {value: '6', label: 'Eletricista => Casa'},
+            {value: '7', label: 'Gesseiro => Casa'},
+          ]}
+          />
+          <Select 
+          name="week_day" 
+          label="Dia da semana" 
+          options={[
+            {value: '0', label: 'Domingo'},
+            {value: '1', label: 'Segunda-feira'},
+            {value: '2', label: 'Terça-feira'},
+            {value: '3', label: 'Quarta-feira'},
+            {value: '4', label: 'Quinta-feira'},
+            {value: '5', label: 'Sexta-feira'},
+            {value: '6', label: 'Sabado'},
+          ]}
+          />
           <Input type="time" name="time" label="Hora" />
 
         </form>
